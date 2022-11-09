ae844029536f174e8d0473642d376dfcafa20b65
Shaatvika <srishaatvika@gmail.com>
2022-11-09 16:02:30 +0530

    Update util.sh

--- util.sh
+++ util.sh
@@ -17,16 +17,23 @@ menu(){
 	echo "7. Show pci devices in tree format"
 	echo "8. Show space available on hard disk"
 	echo "9. Flex arch btw :)"
-	echo "10. Exit"
+	echo "10. Fortune by cows :)"
+	echo "11. Fortune by dragons (they will be fire)"
+	echo "12. To get a hacker-like wallpaper ;)"
+	echo "13. Wanna see a graphic cow?!" 
+	echo "14. For some Colorful greeting!!"
+	echo "15. Channel your inner Joey!!"
+	echo "16. A dad joke from a Turkey"
+	echo "17. Exit"
 	echo
 	echo "----------------------------"
 	echo
-	echo "Please enter option [1 - 10]:"  
+	echo "Please enter option [1 - 17]:"  
 }
 
 option=y
 
-while [ "$option" != "10" ]
+while [ "$option" != "17" ]
 do
 	menu
 
@@ -42,7 +49,14 @@ do
 		7) lspci -vt;;
 		8) df -i;;
 		9) neofetch | lolcat;;
-		10) break;; # exit menu
+		10) fortune|cowsay;;
+		11) fortune|cowsay -f dragons;;
+		12) cmatrix;;
+		13) xcowsay How pretty am I?!!;;
+		14) toilet -f mono12 -F gay "Hello there! Have a nice day!!";;
+		15) cowsay -f eyes How you doin;;
+		16) cowsay -f turkey Why did the police arrest the Turkey? They suspecteed it of 'Fowl play'!;;
+		17) break;;
 		*) echo "Wrong option";; # * means anything else
 esac
 
